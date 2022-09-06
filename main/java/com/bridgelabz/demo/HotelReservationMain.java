a9fecf828664321b300a40d2e3e3699a9e8c092c
unknown <papisettynagendra>
2022-09-06 21:38:27 +0530

    Added: Welcome message

---
+++ main/java/com/bridgelabz/demo/HotelReservationMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.demo;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome To Hotel Reservation");
+    }
+}
