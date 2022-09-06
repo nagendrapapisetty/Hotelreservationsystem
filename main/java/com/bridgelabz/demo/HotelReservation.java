05f7b49d9c44ee762a8ad6df0648aa3692980aaa
unknown <papisettynagendra>
2022-09-06 21:32:49 +0530

    Added: method to add to hotel

---
+++ main/java/com/bridgelabz/demo/HotelReservation.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.demo;
+import java.util.*;
+public class HotelReservation {
+
+    Map<String, Hotel> hotelList = new HashMap<String, Hotel>();
+    Hotel hotels;
+
+
+
+    public boolean addHotel(String hotelName,int rating, double weekdayRateRegular, double weekendRateRegular)
+    {
+        hotels = new Hotel();
+        hotels.setHotelName(hotelName);
+        hotels.setRating(rating);
+        hotels.setWeekdayRegularCustomerPrice(weekdayRateRegular);
+        hotels.setWeekendRegularCustomerPrice(weekendRateRegular);
+
+        hotelList.put(hotelName, hotels);
+        return true;
+    }
+
+
+}
+
