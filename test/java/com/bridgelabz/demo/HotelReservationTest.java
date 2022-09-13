39f4909c887d41ab0875960458fd7662c1aec9f3
unknown <papisettynagendra>
2022-09-14 00:06:51 +0530

    REFACTORED: Test find cheapest hotel for data range

--- test/java/com/bridgelabz/demo/HotelReservationTest.java
+++ test/java/com/bridgelabz/demo/HotelReservationTest.java
@@ -3,50 +3,56 @@ import java.time.LocalDate;
 import java.time.Month;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelReservationTest {
+
 
     @Test
     public void givenHotelDetails_WhenValuesEnteredAreCorrect_ShoulReturnTrue()
     {
-        HotelReservationIF hotelReservation = (HotelReservationIF) new HotelReservation();
+        HotelReservationIF hotelReservation = new HotelReservation();
         hotelReservation.addHotel("Lakewood", 3, 110, 90);
         hotelReservation.addHotel("Bridgewood", 4, 150, 50);
         hotelReservation.addHotel("Ridgewood", 5, 220, 150);
         int hotelListSize = hotelReservation.getHotelListSize();
         hotelReservation.printHotelList();
-        Assert.assertEquals(3, hotelListSize);
+
     }
 
     @Test
     public void givenHotelList_WhenAdded_shouldReturnProperHotelName(){
-        HotelReservationIF hotelReservation = (HotelReservationIF) new HotelReservation();
+        HotelReservationIF hotelReservation = new HotelReservation();
         hotelReservation.addHotel("Bridgewood", 4, 150, 50);
-        String hotelName = hotelReservation.getHotelList().get(0).getHotelName();
-        Assert.assertEquals("Bridgewood", hotelName);
+        String hotelName;
+        hotelName = hotelReservation.getHotelList().get(0).getHotelName();
+        assertEquals("Bridgewood", hotelName);
     }
 
     @Test
     public void givenHotelList_WhenAdded_shouldReturnProperHotelRating(){
-        HotelReservationIF hotelReservation = (HotelReservationIF) new HotelReservation();
+        HotelReservationIF hotelReservation = new HotelReservation();
         hotelReservation.addHotel("Bridgewood", 4, 150, 50);
-        int hotelRating = hotelReservation.getHotelList().get(0).getRating();
-        Assert.assertEquals(4, hotelRating);
+        int hotelRating;
+        hotelRating = hotelReservation.getHotelList().get(0).getRating();
+        assertEquals(4, hotelRating);
     }
 
     @Test
     public void givenHotelList_WhenAdded_shouldReturnProperHotelWeekdayRegularCustomerCost(){
-        HotelReservationIF hotelReservation = (HotelReservationIF) new HotelReservation();
+        HotelReservationIF hotelReservation = new HotelReservation();
         hotelReservation.addHotel("Bridgewood", 4, 150, 50);
         int hotelRegularCustomerCost = (int) hotelReservation.getHotelList().get(0).getWeekdayRegularCustomerCost();
-        Assert.assertEquals(150, hotelRegularCustomerCost);
+        assertEquals(150, hotelRegularCustomerCost);
     }
 
     @Test
     public void givenHotelList_WhenAdded_shouldReturnProperHotelWeekendRegularCustomerCost(){
-        HotelReservationIF hotelReservation = (HotelReservationIF) new HotelReservation();
+        HotelReservationIF hotelReservation = new HotelReservation();
         hotelReservation.addHotel("Bridgewood", 4, 150, 50);
         int hotelRegularCustomerCost = (int) hotelReservation.getHotelList().get(0).getWeekendRegularCustomerCost();
-        Assert.assertEquals(50, hotelRegularCustomerCost);
+        assertEquals(50, hotelRegularCustomerCost);
     }
 
     @Test
@@ -56,8 +62,8 @@ public class HotelReservationTest {
         hotelReservation.addHotel("Lakewood", 3, 110, 90);
         hotelReservation.addHotel("Bridgewood", 4, 160, 50);
         LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);
-        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
-        Hotel hotel = hotelReservation.getCheapestHotel(startDate, endDate);
-        Assert.assertEquals("Lakewood", hotel.getHotelName());
+        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);
+        String hotelName = hotelReservation.getCheapestHotel(startDate, endDate);
+        assertEquals("Lakewood", hotelName);
     }
 }
